79875528f368e3d72dc6b5399ae3e1b58f90eb6a
sodiumspark <8516953+sodiumspark@users.noreply.github.com>
2020-04-10 15:33:12 +1200

    new project setup

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\manas\Desktop\Unitec\WebDevelop_7420\Workspace\IoT_Dashboard\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
