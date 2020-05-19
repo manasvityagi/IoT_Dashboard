471deb76ac8e02f29bbab3e641004a6bfdd034a2
manastyagi <manasvi.tyagi@gmail.com>
2020-05-19 22:57:59 +1200

    reset

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\manas\Desktop\Unitec\WebDevelop_7420\Workspace\IoT_Dashbaord\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
