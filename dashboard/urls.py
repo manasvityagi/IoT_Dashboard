0e2fb26d7b954204e0f2aef7990d3e7b523f3f6a
manastyagi <manasvi.tyagi@gmail.com>
2020-05-21 01:43:36 +1200

    backup/

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/info/', views.info, name='dashboard-info'),
-    path('', views.not_found, name='404')
+    path('', views.info, name='dashboard-info'),
+    # path('', views.not_found, name='404')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+things = [
+    {
+        'name': 'Coffee Machine',
+        'lifetime_used': 152,
+        'mfg_date': '04/12/2018',
+        'last_cleaned': '04/05/2020',
+        'max_lifetime': 1555
+    },
+    {
+        'name': 'Vacuum Cleaner',
+        'lifetime_used': 167,
+        'mfg_date': '04/11/2018',
+        'last_cleaned': '04/05/2020',
+        'max_lifetime': 5000
+    }
+]
+
 
 # Create your views here.
 def info(request):
-    return HttpResponse("<h1>Dashboard Home Info</h1>")
+    context = {
+        'things': things
+    }
+    return render(request, 'dashboard/home.html', context)
 
 
 def not_found(request):
