78fc26a56109a5f2d99ef996e97689d534b6043e
manastyagi <manasvi.tyagi@gmail.com>
2020-06-01 00:44:04 +1200

    backup

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+# TODO Write Unit Test
+# TODO Write Integration Test
+# TODO Demonstrate Test Fixture
+
+
 # Create your tests here.
+from dashboard.models import Address
+
+
+class DashboardModelTest(TestCase):
+    #
+    def test_field(self):
+        address  = Address()
+
