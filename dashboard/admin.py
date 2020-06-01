6a4e2436b068905e84a5672e88d3495dc8b6aa0d
manastyagi <manasvi.tyagi@gmail.com>
2020-06-01 16:11:42 +1200

    models registered in admin page plus customizing further

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,20 +3,24 @@ from django import forms
 from .models import *
 
 
-# Register your models here.
+# Demo of Customizong the admin page
 class CustomAdmin(admin.ModelAdmin):
     extra_field = forms.EmailField()
     search_fields = ('device_model_info',)
-    list_display = ('device_model_info', 'description',)
+    list_display = ('device_model_info', 'description','purchase_date',)
     list_filter = ('installed_home_id',)
     fields = ('device_model_info',)
 
 
 admin.site.register(Thing, CustomAdmin)
-admin.site.register(Manufacturer)
 admin.site.register(Address)
+admin.site.register(Manufacturer)
 admin.site.register(Home)
 admin.site.register(DeviceModels)
 admin.site.register(ValueStream)
 admin.site.register(ServiceProvider)
 admin.site.register(Seller)
+admin.site.register(ServiceDetails)
+admin.site.register(SubscribersList)
+
+
