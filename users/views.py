ac179a5e0f8573d612ba7d7186fcb8e589228805
manastyagi <manasvi.tyagi@gmail.com>
2020-06-02 18:36:17 +1200

    import error fix

--- users/views.py
+++ users/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 
+from .models import SubscribersList
 from .tasks import send_email
-from .myform import CustomRegistrationForm
+from .myform import CustomRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 from.tasks import sleepy
 
