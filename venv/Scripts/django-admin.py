cbb8706faea061fcee18f2763d6b5cf5e79dd948
manastyagi <manasvi.tyagi@gmail.com>
2020-07-02 00:50:01 +1200

    update1

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!w:\workspace\webdev_ds\django\iot_dashboard\venv\scripts\python.exe
+#!W:\workspace\WebDev_DS\django\IoT_Dashboard\venv\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
